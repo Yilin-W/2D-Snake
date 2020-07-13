89953d21a3413d0170d4d6788db322f91cfd3b55
Linda Wei <yilinwei@elasticpath.com>
2020-07-12 20:09:36 -0700

    Draw background, snake head, score, and food on to the canvas

--- snake2D.js
+++ snake2D.js
@@ -32,3 +32,33 @@ let food = {
 // create the score var
 
 let score = 0;
+
+// draw everything to the canvas
+
+function draw(){
+    ctx.drawImage(background,0,0);
+
+    //loop over the snake to draw all the cells
+    for (let i = 0; i < snake.length; i ++){
+        ctx.fillStyle = ( i == 0 || i % 2 == 0) ? "#006400" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+        ctx.strokeStyle = "orange";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    // draw the food icon on the top
+    ctx.drawImage(foodImg, 13.5*box, 0.4*box);
+
+    //draw the actual food
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    //draw the store on the top
+    ctx.fillStyle = "white";
+    ctx.font = "bold 45px Old Standard TT";
+    ctx.fillText(score, 16*box, 1.6*box);
+}
+
+//call draw function every 100 ms
+
+let game = setInterval(draw,100);
