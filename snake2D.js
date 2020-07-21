5d9954a4b561c3831f2af28087b5a16a88f92f2a
Linda Wei <yilinwei@elasticpath.com>
2020-07-20 17:59:09 -0700

    Add eventlistner to move the snake around

--- snake2D.js
+++ snake2D.js
@@ -12,12 +12,15 @@ background.src = "image/background.png";
 const foodImg = new Image();
 foodImg.src = "image/food.png";
 
+const iconImg = new Image();
+iconImg.src = "image/icon.png"
+
 //create the snake, {} declaring an object
 
 let snake = [];
 snake[0] = {
     x : 9 * box,
-    y : 10* box
+    y : 10 * box
 }
 
 // create the food randomly using math. Math random create a number between
@@ -32,6 +35,22 @@ let food = {
 // create the score var
 
 let score = 0;
+
+//control the snake
+let d;
+document.addEventListener("keydown",direction);
+
+function direction(event){
+    if(event.keyCode == 37 && d !="RIGHT"){
+        d = "LEFT";
+    }else if(event.keyCode == 38 && d !="DOWN"){
+        d = "UP";
+    }else if(event.keyCode == 39 && d !="LEFT"){
+        d = "RIGHT";
+    }else if(event.keyCode == 40 && d != "UP"){
+        d = "DOWN";
+    }
+}
 
 // draw everything to the canvas
 
@@ -48,10 +67,31 @@ function draw(){
     }
 
     // draw the food icon on the top
-    ctx.drawImage(foodImg, 13.5*box, 0.4*box);
+    ctx.drawImage(iconImg, 13.5*box, 0.4*box);
 
     //draw the actual food
     ctx.drawImage(foodImg, food.x, food.y);
+
+    //old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //remove the tail
+    snake.pop();
+
+    //which direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+
+    //add new head
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+
+    snake.unshift(newHead);
 
     //draw the store on the top
     ctx.fillStyle = "white";
